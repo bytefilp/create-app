b094e34c5cb90d989333d83fd9c1d061b7f0346a
martinageradams <martinageradams@gmail.com>
2022-03-16 16:02:40 +0800

    feat: ➕ Add recoil as a state management dependency.

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { RecoilRoot } from "recoil";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <RecoilRoot>
+      <App />
+    </RecoilRoot>
   </React.StrictMode>,
   document.getElementById('root'),
 );
